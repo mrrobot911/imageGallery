b55feebf785cf13fd36094530f68e95784b988bb
mrrobot911 <kelwin2705@gmail.com>
2023-09-29 15:59:07 +0300

    fix: clear input

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let data = 'anime';
 let page = 1;
+let render = false;
 // const apiKey = 'Ps0UKzTKZQbNZI8fCPltSBO5ct9U-svvBmxzQX7BDZ4';
 const apiKey = 'ai8aJK-efo0saOGfga1MIjYxIuEhNg7DrOiftfKK3bg';
 
@@ -25,12 +25,15 @@ const buttonCleare = document.createElement('button');
 buttonCleare.className = 'cleareBtn';
 buttonCleare.innerHTML = `<svg viewBox="0 0 24 24" width="24px" height="24px" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
 buttonCleare.addEventListener('click', e => {
+    e.preventDefault();
+    input.value !== '' && render && fetchRandom();
     input.value = '';
+    render = false;
 })
 input.append(buttonSearch);
 searchBlock.addEventListener('submit', e => {
-        e.preventDefault();
-        input.value !== '' && fetchData(input.value);
+    e.preventDefault();
+    input.value !== '' && fetchData(input.value);
 });
 searchBlock.append(input, buttonSearch, buttonCleare);
 header.append(elName,searchBlock);
@@ -56,6 +59,13 @@ rsImg.className = 'rs';
 rs.append(rsImg);
 footer.append(author, year, rs);
 
+async function fetchRandom() {
+    const url = `https://api.unsplash.com/photos/?client_id=${apiKey}`;
+    const response = await fetch(url);
+    const result = await response.json();
+    render = true;
+    fetchData(result);
+}
 async function fetchData(data){
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${data}&client_id=${apiKey}`;
     const response = await fetch(url);
@@ -75,9 +85,8 @@ function showData(data){
         arr.push(imageContainer);
     });
     gallery.replaceChildren(...arr);
-    
 }
-fetchData(data);
+fetchRandom();
 body.append(header, gallery, footer);
 
 
