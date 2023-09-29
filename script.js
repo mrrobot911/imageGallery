5e4dc3faa51c93853c158d5618344324de37efec
mrrobot911 <kelwin2705@gmail.com>
2023-09-29 19:32:26 +0300

    fix:  buttons

--- script.js
+++ script.js
@@ -24,17 +24,19 @@ buttonSearch.type = 'submit';
 buttonSearch.innerHTML = `<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`;
-const buttonCleare = document.createElement('button');
+const buttonCleare = document.createElement('span');
 buttonCleare.className = 'cleareBtn';
 buttonCleare.innerHTML = `<svg viewBox="0 0 24 24" width="24px" height="24px" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
-buttonCleare.addEventListener('click', e => {
+buttonCleare.addEventListener('click', (e) => {
     e.preventDefault();
     input.value !== '' && render && fetchRandom();
     input.value = '';
     render = false;
+    buttonCleare.style.display = 'none';
 });
+buttonCleare.style.display = 'none';
 input.append(buttonSearch);
-searchBlock.addEventListener('submit', e => {
+searchBlock.addEventListener('submit', (e) => {
     e.preventDefault();
     input.value !== '' && fetchData(input.value);
 });
@@ -62,17 +64,17 @@ rsImg.className = 'rs';
 rs.append(rsImg);
 footer.append(author, year, rs);
 
-async function fetchRandom() {
+const fetchRandom =  async() => {
     const url = `https://api.unsplash.com/photos/?client_id=${apiKey}`;
     const response = await fetch(url);
     const result = await response.json();
-    render = true;
-    fetchData(result);
+    showData(result);
 }
-async function fetchData(data){
+const fetchData = async(data) =>{
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${data}&client_id=${apiKey}`;
     const response = await fetch(url);
     const result = await response.json();
+    render = true;
     showData(result.results) 
 }
 function showData(data){
