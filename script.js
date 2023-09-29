d70cc40237f001eacf67e5eaf5a7825af1030c9a
mrrobot911 <kelwin2705@gmail.com>
2023-09-29 22:15:35 +0300

    fix: clear input

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ buttonSearch.innerHTML = `<svg width="24px" height="24px" viewBox="0 0 24 24" fi
 const buttonCleare = document.createElement('span');
 buttonCleare.className = 'cleareBtn';
 buttonCleare.innerHTML = `<svg viewBox="0 0 24 24" width="24px" height="24px" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M16 8L8 16M8.00001 8L16 16M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`
+buttonCleare.title = 'при нажатии обнулится запрос';
 buttonCleare.addEventListener('click', (e) => {
     e.preventDefault();
     input.value !== '' && render && fetchRandom();
